484e9b47c71d1ada0dd3f911c062136de640bc59
Walter Guillermo Mendez Villanueva <walter.mendez.michelada@kueski.com>
2022-08-28 18:21:21 -0500

    Add views

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+gem "devise", "~> 4.8"
 gem 'will_paginate', '~> 3.3'
 gem "delayed_job_active_record", "~> 4.1"
 gem 'daemons'
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  # Comment out to use React
+  root to: 'invoices#index'
+
+  resources :invoices
+  resources :people, only: :show
+
+  # Uncomment to use React
+  # root to: 'react_app#index'
+  # get '*path', to: 'react_app#index'
 end
